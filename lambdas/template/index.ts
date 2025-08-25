2bc453447af5e9f82ea39ae13cc5631c9c03ae92
AdrianoVitoriano <adrianovitorianosilva@gmail.com>
2025-08-25 19:43:08 -0300

    Refactor handler message in index.ts and add tsconfig.json for TypeScript configuration

--- lambdas/template/index.ts
+++ lambdas/template/index.ts
@@ -1,9 +1,8 @@
 export const handler = async (event: any = {}): Promise<any> => {
-
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Lambda Template created successfully!'
+            message: 'Order created successfully!',
         }),
     };
 }
